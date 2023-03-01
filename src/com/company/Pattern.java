2d347267c450c0d29fef1f58a765f1382fce2d42
sajibsaha4647 <sksaha4648@gmail.com>
2023-03-01 16:35:21 +0600

    added right angle pattern

--- src/com/company/Pattern.java
+++ src/com/company/Pattern.java
@@ -9,11 +9,28 @@ public class Pattern {
         System.out.println("enter a valid number");
         number = input.nextInt();
 
+//Top to bottom right angle
+// 1 
+// 12 
+// 123 
+// 1234 
+// 12345 
         for(int row = 1; row<=number;row++){
             for(int col = 1 ; col<=row;col++){
-                System.out.print(row);
+                System.out.print(col);
             }
             System.out.println(" ");
         }
+
+        System.out.println(" ");
+
+        for(int row = number;row >=1;row--){
+            for(int col = 1; col<=row;col++){
+                System.out.print(col);
+            }
+            System.out.println(" ");
+        }
+
+
     }
 }
